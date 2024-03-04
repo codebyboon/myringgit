31944f0282111d3c278cdc6290351a6e06c692fc
Saw Yee Boon <codebyboon@gmail.com>
2024-03-04 22:41:37 +0800

    Initialize the project for financial management app

---
+++ .next/static/webpack/app/page.9b885a55332a9667.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CProjects%5Cfinancial-management-app%5Csrc%5Ccomponents%5Cnavbar%5Cmode-toggle.tsx&modules=D%3A%5CProjects%5Cfinancial-management-app%5Csrc%5Ccomponents%5Cui%5Ccollapsible.tsx&server=false!":
+/*!*****************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CProjects%5Cfinancial-management-app%5Csrc%5Ccomponents%5Cnavbar%5Cmode-toggle.tsx&modules=D%3A%5CProjects%5Cfinancial-management-app%5Csrc%5Ccomponents%5Cui%5Ccollapsible.tsx&server=false! ***!
+  \*****************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/components/navbar/mode-toggle.tsx */ \"(app-pages-browser)/./src/components/navbar/mode-toggle.tsx\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/components/ui/collapsible.tsx */ \"(app-pages-browser)/./src/components/ui/collapsible.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1EJTNBJTVDUHJvamVjdHMlNUNmaW5hbmNpYWwtbWFuYWdlbWVudC1hcHAlNUNzcmMlNUNjb21wb25lbnRzJTVDbmF2YmFyJTVDbW9kZS10b2dnbGUudHN4Jm1vZHVsZXM9RCUzQSU1Q1Byb2plY3RzJTVDZmluYW5jaWFsLW1hbmFnZW1lbnQtYXBwJTVDc3JjJTVDY29tcG9uZW50cyU1Q3VpJTVDY29sbGFwc2libGUudHN4JnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEsd01BQXFIO0FBQ3JIIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ODJjMCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkQ6XFxcXFByb2plY3RzXFxcXGZpbmFuY2lhbC1tYW5hZ2VtZW50LWFwcFxcXFxzcmNcXFxcY29tcG9uZW50c1xcXFxuYXZiYXJcXFxcbW9kZS10b2dnbGUudHN4XCIpO1xuaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJEOlxcXFxQcm9qZWN0c1xcXFxmaW5hbmNpYWwtbWFuYWdlbWVudC1hcHBcXFxcc3JjXFxcXGNvbXBvbmVudHNcXFxcdWlcXFxcY29sbGFwc2libGUudHN4XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CProjects%5Cfinancial-management-app%5Csrc%5Ccomponents%5Cnavbar%5Cmode-toggle.tsx&modules=D%3A%5CProjects%5Cfinancial-management-app%5Csrc%5Ccomponents%5Cui%5Ccollapsible.tsx&server=false!\n"));
+
+/***/ })
+
+});
